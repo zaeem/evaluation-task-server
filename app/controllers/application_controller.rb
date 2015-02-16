7005537f3ca60f269d95c091222f11fca0f89a85
Im Genius <zaeem.asif@gmail.com>
2015-02-16 21:24:00 +0500

    s

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
+  # self.responder = ApplicationResponder
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token
+  # respond_to :json
+
 end
