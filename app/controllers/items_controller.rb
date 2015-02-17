2b54e8990d0436fe1f4f9fc8f734ace0cf5dbf23
Im Genius <zaeem.asif@gmail.com>
2015-02-17 15:49:03 +0500

    Server side app

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,10 +20,11 @@ class ItemsController < ApplicationController
         name = params[:search_name]
         name_res = /#{name}/i.match(item.name).present?
       end
+      
       if params[:ids].present?
         id_res = params[:ids].index(item.id.to_s).present? 
       end      
-      # if name_res && rem_res
+      
       if name_res || id_res
         updated_items << {id: item.id, name: item.name, description: item.description, image: item.image }
       end
