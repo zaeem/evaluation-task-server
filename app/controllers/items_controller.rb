73a777151d42a6710f883defa0a35765d6358da0
Im Genius <zaeem.asif@gmail.com>
2015-02-17 12:02:50 +0500

    s

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,34 @@
 class ItemsController < ApplicationController
+  before_action :load_items
   def index
-    return render json: Item.all.to_json
+    return render json: @items.to_json
+  end
+
+  def item_names
+    return render json: @items.map(&:name)
+  end
+
+  def filtered_items
+    updated_items = []
+    @items.each do |item|
+
+      if params[:search_name].present?  
+        name = params[:search_name]
+        name_res = /#{name}/i.match(item.name).present?
+      end
+      if params[:ids].present?
+        id_res = params[:ids].index(item.id.to_s).present? 
+      end      
+      # if name_res && rem_res
+      if name_res || id_res
+        updated_items << {id: item.id, name: item.name, description: item.description, image: item.image }
+      end
+    end
+    render json: updated_items
+  end
+  
+  private
+  def load_items
+    @items = Item.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :items
+  resources :items do
+    collection do
+      get :item_names
+      get :filtered_items
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
